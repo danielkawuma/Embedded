875f73f5c2cbb3f83134d8bb5f08156bd3f8bce8
daniel <danielkawuma@gmail.com>
2018-06-05 13:10:26 +0300

    This is it

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Animal{
+	private String name;
+	public void Setname(String name){
+	this.name = name;
+}
+	public String getName(){
+	System.out.println("Its a change");
+	return name;
+}
+
+}
